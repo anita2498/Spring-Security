2681cc825277663e2d2c1081c8f554e5e9b6a94c
anita2498 <anitaputhenpurayil@gmail.com>
2021-02-15 12:43:17 -0500

    project complete

---
+++ src/main/java/com/hcl/SpringSecurity1/MyUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.hcl.SpringSecurity1;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		Optional<User> user =userRepository.findByUsername(username);
+		if(user==null)
+			throw new UsernameNotFoundException("User 404");
+		
+		
+		
+		
+		return user.map(UserPrincipal::new).get();
+	}
+
+}
